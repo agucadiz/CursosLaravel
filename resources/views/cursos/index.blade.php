c2afa9e63a1cfc34a734f3cdee286e1130cc96a7
agucadiz <agustin.pedrote@iesdonana.org>
2022-12-27 20:26:40 +0100

    CRUD read

--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -10,6 +10,18 @@
 
 <body>
     <h1>Bienvenido a la página principal de cursos.</h1>
+
+    <a href="{{route('cursos.create')}}">Crear curso</a>
+
+    <ul>
+        @foreach ($cursos as $curso )
+            <li>
+                <a href="{{route('cursos.show', $curso->id)}}">{{$curso->name}}</a>
+            </li>
+        @endforeach
+    </ul>
+
+    {{$cursos->links()}}
 </body>
 
 </html>
--- resources/views/cursos/show.blade.php
+++ resources/views/cursos/show.blade.php
@@ -9,7 +9,13 @@
 </head>
 
 <body>
-    <h1>Bienvenido al curso de <?php echo $curso ?>.</h1>
+    <h1>Bienvenido al curso de <?php echo $curso->name ?></h1>
+
+    <a href="{{route('cursos.index')}}">Volver a cursos</a>
+
+    <p><strong>Categoria:</strong>{{$curso->category}}</p>
+
+    <p>{{$curso->description}}</p>
 </body>
 
 </html>
