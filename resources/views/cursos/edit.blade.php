c3cf61f96613b362320c1bbb12370bc563d403d5
agucadiz <agustin.pedrote@iesdonana.org>
2022-12-28 10:47:49 +0100

    CRUD agregar y actualizar

--- resources/views/cursos/edit.blade.php
+++ resources/views/cursos/edit.blade.php
@@ -5,34 +5,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create</title>
+    <title>Edit</title>
 </head>
 
 <body>
-    <h1>En esta página podrás crear un curso</h1>
+    <h1>En esta página podrás editar un curso</h1>
 
-    <form action="{{route('cursos.store')}}" method="POST">
+    <form action="{{route('cursos.update', $curso)}}" method="POST">
+
         @csrf
+
+        @method('put')
 
         <label>
             Nombre:
             <br>
-            <input type="text" name="name">
+            <input type="text" name="name" value="{{$curso->name}}">
         </label>
+
         <br>
         <label>
             Descripción:
             <br>
-            <textarea name="description" rows="5"></textarea>
+            <textarea name="description" rows="5">{{$curso->description}}</textarea>
         </label>
+
         <br>
         <label>
             Categoría:
             <br>
-            <input type="text" name="category">
+            <input type="text" name="category" value="{{$curso->category}}">
         </label>
+
         <br><br>
-        <button type="submit">Enviar</button>
+        <button type="submit">Actualizar</button>
 
     </form>
 </body>
--- resources/views/cursos/show.blade.php
+++ resources/views/cursos/show.blade.php
@@ -5,13 +5,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nombre</title>
+    <title>Show</title>
 </head>
 
 <body>
     <h1>Bienvenido al curso de <?php echo $curso->name ?></h1>
 
     <a href="{{route('cursos.index')}}">Volver a cursos</a>
+    <br>
+    <a href="{{route('cursos.edit', $curso)}}">Editar curso</a>
 
     <p><strong>Categoria:</strong>{{$curso->category}}</p>
 
